10048fa4d486bcbbd4638eb35b7966fc0f45ebf8
asel hidayat sjamhars <asel.hidayat@gmail.com>
2026-05-14 23:52:28 +0700

    add window picker

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,12 +6,27 @@ vim.pack.add {
   { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/s1n7ax/nvim-window-picker',
+}
+
+require('window-picker').setup {
+  hint = 'floating-big-letter',
+  selection_chars = 'FJDKSLA;CMRUEIWOQP',
+  filter_rules = {
+    include_current_win = false,
+    autoselect_one = true,
+    bo = {
+      filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+      buftype = { 'terminal', 'quickfix' },
+    },
+  },
 }
 
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
 require('neo-tree').setup {
   close_if_last_window = true,
+  open_files_using_window_picker = true,
   window = {
     position = 'right',
     width = 60,
