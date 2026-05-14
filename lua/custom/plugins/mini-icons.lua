ccaa99f4efb434bc2c7d01ed9e7ba9d0920c63da
asel hidayat sjamhars <asel.hidayat@gmail.com>
2026-05-14 23:11:04 +0700

    integrate mini icons with neo tree

---
+++ lua/custom/plugins/mini-icons.lua
@@ -0,0 +1,5 @@
+-- mini.icons is part of mini.nvim (already installed)
+-- mock_nvim_web_devicons makes it act as a drop-in replacement so plugins
+-- like neo-tree that integrate with nvim-web-devicons get mini.icons instead.
+require('mini.icons').setup()
+MiniIcons.mock_nvim_web_devicons()
